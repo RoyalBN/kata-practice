97ab74027892c307bd7dee53e4fe4e9f4d39edd7
RoyalBN <sebastien.samson22@gmail.com>
2025-04-28 21:09:52 +0200

    :memo: docs: Listed tests to implement + updated README.md file

--- ChristmasLightsKata/src/test/java/org/example/ChristmasLightsTest.java
+++ ChristmasLightsKata/src/test/java/org/example/ChristmasLightsTest.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,6 +18,9 @@ import static org.junit.jupiter.api.Assertions.*;
  *  - Each coordinate pair represents opposite corners of a rectangle, inclusive
  *  /!| The lights all start turned off.
  *
+ *  - [ON] before a test name concerns the Turn-ON functionality
+ *  - [OFF] before a test name concerns the Turn-OFF functionality
+ *  - [TOGGLE] before a test name concerns the TOGGLE functionality
  */
 
 class ChristmasLightsTest {
@@ -23,4 +28,22 @@ class ChristmasLightsTest {
     @BeforeEach
     void setUp() {
     }
+
+    // All lights should be turned off at start
+
+    // [ON] Turn on a single light
+    // [ON] Turn on a single row of lights
+    // [ON] Turn on a single column of lights
+    // [ON] Turn on all lights
+
+    // [OFF] Turn off a single light
+    // [OFF] Turn off a single row of lights
+    // [OFF] Turn off a single column of lights
+    // [OFF] Turn off all lights
+
+    // [TOGGLE] Toggle a single light
+    // [TOGGLE] Toggle a single row of lights
+    // [TOGGLE] Toggle a single column of lights
+    // [TOGGLE] Toggle all lights
+
 }
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# kata-practice
+# Welcome to my `kata-practice` repository
+
+The main goal of this repository was to practice various Coding Katas in order to improve my skills and adopt the best practices in software development, such as TDD, SOLID Principles, Refactoring, and Clean Code.
+
+Furthermore, I strongly believe that knowledge should be shared. That's why I've made this repository public. By contributing to it, I hope to provide useful insights, share the learning process, and encourage others to participate in the journey of continuous improvement. Whether you're a beginner or an experienced developer, feel free to explore, learn, and share your own perspectives and improvements.
+
+To make it easier for others to follow my thought process, I've structured my commits in small, clear steps. Each commit represents a logical development milestone, making it easier to compare, understand the changes, and follow the evolution of the solution. This way, anyone can dive into the repository and clearly see the progression, improvements, and reasoning behind each step.
+
+Let's grow together, one Kata at a time!
